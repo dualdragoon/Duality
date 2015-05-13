f8b72049abfd98be60bcf62563320012f64b73c7
dualdragoon <hosleraaron06@gmail.com>
2015-05-12 23:02:28 -0400

    Initial workings. Reworked to work with outside circle.

---
+++ Duality/Circle.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Input;
+
+namespace Duality
+{
+    /// <summary>
+    /// Represents a 2D circle.
+    /// </summary>
+    public struct Circle
+    {
+        /// <summary>
+        /// Backing store for Center.
+        /// </summary>
+        private Vector2 center;
+
+        /// <summary>
+        /// Center position of the circle.
+        /// </summary>
+        public Vector2 Center
+        {
+            get { return center; }
+            set { center = value; }
+        }
+
+        /// <summary>
+        /// Diameter of the circle.
+        /// </summary>
+        public float Diameter;
+
+        /// <summary>
+        /// Constructs a new circle.
+        /// </summary>
+        public Circle(Vector2 position, float diameter)
+        {
+            center = position;
+            Diameter = diameter;
+        }
+
+        /// <summary>
+        /// Determines if a circle intersects a rectangle.
+        /// </summary>
+        /// <returns>True if the circle and rectangle overlap. False otherwise.</returns>
+        public bool Intersects(Rectangle rectangle)
+        {
+            Vector2 v = new Vector2(MathHelper.Clamp(Center.X, rectangle.Left, rectangle.Right),
+                                    MathHelper.Clamp(Center.Y, rectangle.Top, rectangle.Bottom));
+
+            Vector2 direction = Center - v;
+            float distanceSquared = direction.LengthSquared();
+
+            return ((distanceSquared > 0) && (distanceSquared < (Diameter / 2) * (Diameter / 2)));
+        }
+
+        /// <summary>
+        /// Determines if a circle contains the mouse.
+        /// </summary>
+        /// <returns>True if the mouse is within the circle. False otherwise</returns>
+        public bool Intersects(MouseState mouse)
+        {
+            Vector2 v = new Vector2(MathHelper.Clamp(Center.X, mouse.X, mouse.X),
+                                    MathHelper.Clamp(Center.Y, mouse.Y, mouse.Y));
+
+            Vector2 direction = Center - v;
+            float distanceSquared = direction.LengthSquared();
+
+            return ((distanceSquared > 0) && (distanceSquared < (Diameter / 2) * (Diameter / 2)));
+        }
+    }
+}
---
+++ Duality/MenuButton.cs
@@ -0,0 +1,195 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+
+namespace Duality.MenuSystem
+{
+    public struct MenuButton
+    {
+        private enum ButtonType { Rectangle, Circle };
+
+        private bool buttonState { get; set; }
+        private float diameter { get; set; }
+        private int bNum { get; set; }
+        private ButtonType type { get; set; }
+        private MouseState mouseState { get; set; }
+        private Texture2D button0 { get; set; }
+
+        /// <summary>
+        /// Backing store for Collision.
+        /// </summary>
+        private Rectangle collision;
+
+        /// <summary>
+        /// Rectangle struct.
+        /// </summary>
+        public Rectangle Collision
+        {
+            get { return collision; }
+            set { collision = value; }
+        }
+
+        /// <summary>
+        /// Backing store for Circle.
+        /// </summary>
+        private Circle circle;
+
+        /// <summary>
+        /// Circle struct.
+        /// </summary>
+        public Circle Circle
+        {
+            get { return circle; }
+            set { circle = value; }
+        }
+
+        /// <summary>
+        /// Backing Stores for textures.
+        /// </summary>
+        private Texture2D button1, button2;
+
+        /// <summary>
+        /// Set Unpressed Button Texture.
+        /// </summary>
+        public Texture2D UnpressedButton
+        {
+            set { button1 = value; }
+        }
+
+        /// <summary>
+        /// Set Hovered Button Texture.
+        /// </summary>
+        public Texture2D HoveredButton
+        {
+            set { button2 = value; }
+        }
+
+        /// <summary>
+        /// Backing Store for Center of circle.
+        /// </summary>
+        private Vector2 center;
+
+        public Vector2 Center
+        {
+            get { return center; }
+            set { center = value; }
+        }
+
+        /// <summary>
+        /// Creates a new button for the menu.
+        /// </summary>
+        /// <param name="position">Position of top left corner.</param>
+        /// <param name="width">Width of button in pixels.</param>
+        /// <param name="height">Height of button in pixels.</param>
+        /// <param name="buttonNum">Number button uses to identify.</param>
+        /// <param name="mouse">Mouse state for detection.</param>
+        /// <param name="buttonNorm">Ordinary button state.</param>
+        /// <param name="buttonHov">Hovered button state.</param>
+        public MenuButton(Vector2 position, int width, int height, int buttonNum, MouseState mouse, Texture2D buttonNorm, Texture2D buttonHov)
+            : this()
+        {
+            center = Vector2.Zero;
+            collision = new Rectangle((int)position.X, (int)position.Y, width, height);
+            mouseState = mouse;
+            button1 = buttonNorm;
+            button2 = buttonHov;
+            bNum = buttonNum;
+            type = ButtonType.Rectangle;
+        }
+
+        /// <summary>
+        /// Creates a new circular button for the menu.
+        /// </summary>
+        /// <param name="centerPosition">The center position of the circle.</param>
+        /// <param name="cirlceRadius">The radius of the circle.</param>
+        /// <param name="buttonNum">Number button uses to identify.</param>
+        /// <param name="mouse">Mouse state for detection.</param>
+        /// <param name="buttonNorm">Ordinary button state.</param>
+        /// <param name="buttonHov">Hovered button state.</param>
+        public MenuButton(Vector2 centerPosition, float circleDiameter, int buttonNum, MouseState mouse, Texture2D buttonNorm, Texture2D buttonHov)
+            : this()
+        {
+            collision = Rectangle.Empty;
+            center = centerPosition;
+            diameter = circleDiameter;
+            mouseState = mouse;
+            button1 = buttonNorm;
+            button2 = buttonHov;
+            bNum = buttonNum;
+            type = ButtonType.Circle;
+        }
+
+        public bool getButtonState()
+        {
+            switch (type)
+            {
+                case ButtonType.Rectangle:
+                    if (collision.Contains(mouseState.X, mouseState.Y))
+                    {
+                        button0 = button2;
+                        if (mouseState.LeftButton == ButtonState.Pressed)
+                        {
+                            buttonState = true;
+                        }
+                    }
+                    else
+                    {
+                        button0 = button1;
+                        buttonState = false;
+                    }
+                    return buttonState;
+
+                case ButtonType.Circle:
+                    circle = new Circle(center, diameter);
+                    if (circle.Intersects(mouseState))
+                    {
+                        button0 = button2;
+                        if (mouseState.LeftButton == ButtonState.Pressed)
+                        {
+                            buttonState = true;
+                        }
+                    }
+                    else
+                    {
+                        button0 = button1;
+                        buttonState = false;
+                    }
+                    return buttonState;
+
+                default:
+                    return buttonState;
+            }
+        }
+
+        public int getButtonNum()
+        {
+            return bNum;
+        }
+
+        public Vector2 getPosition()
+        {
+            switch (type)
+            {
+                case ButtonType.Circle:
+                    circle = new Circle(center, diameter);
+                    return center - new Vector2(diameter / 2);
+                case ButtonType.Rectangle:
+                    return new Vector2(Collision.X, Collision.Y);
+                default:
+                    return Vector2.Zero;
+            }
+        }
+
+        public Texture2D getTexture()
+        {
+            return button0;
+        }
+    }
+}
