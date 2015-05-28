c3fdb20c4ba5705b4c72f93c0b54b14a351bac25
dualdragoon <hosleraaron06@gmail.com>
2015-05-28 18:26:30 -0400

    Finished Floating Rectangle.  For now....

--- Duality/FloatingRectangle.cs
+++ Duality/FloatingRectangle.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using Microsoft.Xna.Framework;
@@ -8,31 +10,10 @@ namespace Duality
 {
     public struct FloatingRectangle
     {
-        private float x, y, width, height, left = 0, right = 0, top = 0, bottom = 0;
+        [SuppressMessage("Microsoft.Design", "CA1051:DoNotDeclareVisibleInstanceFields")]
+        public float X, Y, Width, Height;
 
-        public float X
-        {
-            get { return x; }
-            set { x = value; }
-        }
-
-        public float Y
-        {
-            get { return y; }
-            set { y = value; }
-        }
-
-        public float Width
-        {
-            get { return width; }
-            set { width = value; }
-        }
-
-        public float Height
-        {
-            get { return height; }
-            set { height = value; }
-        }
+        private static FloatingRectangle _empty = default(FloatingRectangle);
 
         public float Left
         {
@@ -54,37 +35,271 @@ namespace Duality
             get { return Y + Height; }
         }
 
-        public Vector2 Position
+        public Vector2 Location
         {
             get { return new Vector2(X, Y); }
             set { X = value.X; Y = value.Y; }
         }
 
-        public FloatingRectangle(float X, float Y, float width, float height)
+        public Vector2 Center
         {
-            this.x = X;
-            this.y = Y;
-            this.width = width;
-            this.height = height;
+            get { return new Vector2(X + (Width / 2), Y + (Height / 2)); }
         }
 
-        public bool Intersect(FloatingRectangle floatingRectangle)
+        public static FloatingRectangle Empty
         {
-            if ((this.Bottom > floatingRectangle.Top && this.Bottom < floatingRectangle.Bottom) || (this.Top < floatingRectangle.Bottom && this.Top > floatingRectangle.Top) || this.Top == floatingRectangle.Top || this.Bottom == floatingRectangle.Bottom)
+            get { return FloatingRectangle._empty; }
+        }
+
+        public bool IsEmpty
+        {
+            get { return this.Width == 0 && this.Height == 0 && this.X == 0 && this.Y == 0; }
+        }
+
+        public FloatingRectangle(float x, float y, float width, float height)
+        {
+            this.X = x;
+            this.Y = y;
+            this.Width = width;
+            this.Height = height;
+        }
+
+        public void Offset(Vector2 amount)
+        {
+            X += amount.X;
+            Y += amount.Y;
+        }
+
+        public void Offset(Point amount)
+        {
+            X += amount.X;
+            Y += amount.Y;
+        }
+
+        public void Offset(float offsetX, float offsetY)
+        {
+            X += offsetX;
+            Y += offsetY;
+        }
+
+        public void Offset(int offsetX, int offsetY)
+        {
+            X += offsetX;
+            Y += offsetY;
+        }
+
+        public void Inflate(float horizontalAmount, float verticalAmount)
+        {
+            X -= horizontalAmount;
+            Y -= verticalAmount;
+            Width += horizontalAmount * 2;
+            Height += verticalAmount * 2;
+        }
+
+        public void Inflate(int horizontalAmount, int verticalAmount)
+        {
+            X -= horizontalAmount;
+            Y -= verticalAmount;
+            Width += horizontalAmount * 2;
+            Height += verticalAmount * 2;
+        }
+
+        public bool Contains(float x, float y)
+        {
+            return Left <= x && x < Right && Top <= y && y < Bottom;
+        }
+
+        public bool Contains(int x, int y)
+        {
+            return Left <= x && x < Right && Top <= y && y < Bottom;
+        }
+
+        public bool Contains(Vector2 value)
+        {
+            return Left <= value.X && value.X < Right && Top <= value.Y && value.Y < Bottom;
+        }
+
+        public bool Contains(Point value)
+        {
+            return Left <= value.X && value.X < Right && Top <= value.Y && value.Y < Bottom;
+        }
+
+        public void Contains(ref Vector2 value, out bool result)
+        {
+            result = (Left <= value.X && value.X < Right && Top <= value.Y && value.Y < Bottom);
+        }
+
+        public void Contains(ref Point value, out bool result)
+        {
+            result = (Left <= value.X && value.X < Right && Top <= value.Y && value.Y < Bottom);
+        }
+
+        public bool Contains(FloatingRectangle value)
+        {
+            return Left <= value.X && value.Right <= Right && Top <= value.Y && value.Bottom <= Bottom;
+        }
+
+        public bool Contains(Rectangle value)
+        {
+            return Left <= value.X && value.Right <= Right && Top <= value.Y && value.Bottom <= Bottom;
+        }
+
+        public void Contains(ref FloatingRectangle value, out bool result)
+        {
+            result = (Left <= value.X && value.Right <= Right && Top <= value.Y && value.Bottom <= Bottom);
+        }
+
+        public void Contains(ref Rectangle value, out bool result)
+        {
+            result = (Left <= value.X && value.Right <= Right && Top <= value.Y && value.Bottom <= Bottom);
+        }
+
+        public bool Intersects(FloatingRectangle value)
+        {
+            return value.Left < Right && Left < value.Right && value.Top < Bottom && Top < value.Bottom;
+        }
+
+        public bool Intersects(Rectangle value)
+        {
+            return value.Left < Right && Left < value.Right && value.Top < Bottom && Top < value.Bottom;
+        }
+
+        public void Intersects(ref FloatingRectangle value, out bool result)
+        {
+            result = (value.Left < Right && Left < value.Right && value.Top < Bottom && Top < value.Bottom);
+        }
+
+        public void Intersects(ref Rectangle value, out bool result)
+        {
+            result = (value.Left < Right && Left < value.Right && value.Top < Bottom && Top < value.Bottom);
+        }
+
+        public static FloatingRectangle Intersect(FloatingRectangle value1, FloatingRectangle value2)
+        {
+            float num = value1.Right;
+            float num2 = value2.Right;
+            float num3 = value1.Bottom;
+            float num4 = value2.Bottom;
+            float num5 = (value1.X > value2.X) ? value1.X : value2.X;
+            float num6 = (value1.Y > value2.Y) ? value1.Y : value2.Y;
+            float num7 = (num < num2) ? num : num2;
+            float num8 = (num3 < num4) ? num3 : num4;
+            FloatingRectangle result;
+            if (num7 > num5 && num8 > num6)
             {
-                if (this.Right > floatingRectangle.Left || this.Left < floatingRectangle.Right)
-                {
-                    return true;
-                }
-                else
-                {
-                    return false;
-                }
+                result.X = num5;
+                result.Y = num6;
+                result.Width = num7 - num5;
+                result.Height = num8 - num6;
             }
             else
             {
-                return false;
+                result.X = 0;
+                result.Y = 0;
+                result.Width = 0;
+                result.Height = 0;
             }
+            return result;
+        }
+
+        public static void Intersect(ref FloatingRectangle value1, ref FloatingRectangle value2, out FloatingRectangle result)
+        {
+            float num = value1.Right;
+            float num2 = value2.Right;
+            float num3 = value1.Bottom;
+            float num4 = value2.Bottom;
+            float num5 = (value1.X > value2.X) ? value1.X : value2.X;
+            float num6 = (value1.Y > value2.Y) ? value1.Y : value2.Y;
+            float num7 = (num < num2) ? num : num2;
+            float num8 = (num3 < num4) ? num3 : num4;
+            if (num7 > num5 && num8 > num6)
+            {
+                result.X = num5;
+                result.Y = num6;
+                result.Width = num7 - num5;
+                result.Height = num8 - num6;
+                return;
+            }
+            result.X = 0;
+            result.Y = 0;
+            result.Width = 0;
+            result.Height = 0;
+        }
+
+        public static FloatingRectangle Union(FloatingRectangle value1, FloatingRectangle value2)
+        {
+            float num = value1.Right;
+            float num2 = value2.Right;
+            float num3 = value1.Bottom;
+            float num4 = value2.Bottom;
+            float num5 = (value1.X < value2.X) ? value1.X : value2.X;
+            float num6 = (value1.Y < value2.Y) ? value1.Y : value2.Y;
+            float num7 = (num > num2) ? num : num2;
+            float num8 = (num3 > num4) ? num3 : num4;
+            FloatingRectangle result;
+            result.X = num5;
+            result.Y = num6;
+            result.Width = num7 - num5;
+            result.Height = num8 - num6;
+            return result;
+        }
+
+        public static void Union(ref FloatingRectangle value1, ref FloatingRectangle value2, out FloatingRectangle result)
+        {
+            float num = value1.Right;
+            float num2 = value2.Right;
+            float num3 = value1.Bottom;
+            float num4 = value2.Bottom;
+            float num5 = (value1.X < value2.X) ? value1.X : value2.X;
+            float num6 = (value1.Y < value2.Y) ? value1.Y : value2.Y;
+            float num7 = (num > num2) ? num : num2;
+            float num8 = (num3 > num4) ? num3 : num4;
+            result.X = num5;
+            result.Y = num6;
+            result.Width = num7 - num5;
+            result.Height = num8 - num6;
+        }
+
+        public bool Equals(FloatingRectangle other)
+        {
+            return Left == other.Left && Top == other.Top && Width == other.Width && Height == other.Height;
+        }
+
+        public override bool Equals(object obj)
+        {
+            bool result = false;
+            if (obj is FloatingRectangle)
+            {
+                result = Equals((FloatingRectangle)obj);
+            }
+            return result;
+        }
+
+        public override string ToString()
+        {
+            CultureInfo currentCulture = CultureInfo.CurrentCulture;
+            return string.Format(currentCulture, "{{X:{0} Y:{1} Width:{2} Height:{3}}}", new object[]
+            {
+                X.ToString(currentCulture),
+                Y.ToString(currentCulture),
+                Width.ToString(currentCulture),
+                Height.ToString(currentCulture)
+            });
+        }
+
+        public override int GetHashCode()
+        {
+            return X.GetHashCode() + Y.GetHashCode() + Width.GetHashCode() + Height.GetHashCode();
+        }
+
+        public static bool operator ==(FloatingRectangle a, FloatingRectangle b)
+        {
+            return a.X == b.X && a.Y == b.Y && a.Width == b.Width && a.Height == b.Height;
+        }
+
+        public static bool operator !=(FloatingRectangle a, FloatingRectangle b)
+        {
+            return a.X != b.X || a.Y != b.Y || a.Width != b.Width || a.Height != b.Height;
         }
     }
 }
