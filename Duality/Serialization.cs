7778dcf4081ec0c11c011f3241681067a4d29e53
dualdragoon <hosleraaron06@gmail.com>
2015-06-04 20:19:35 -0400

    changed calss name

--- Duality/Serialization.cs
+++ Duality/Serialization.cs
@@ -8,7 +8,7 @@ using System.Threading.Tasks;
 
 namespace Duality.Records
 {
-    class Serializer
+    class Serialization
     {
         public static TData DeserializeFromString<TData>(string settings)
         {
