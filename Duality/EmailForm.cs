697dffd37b6b699cc4a2126c6b4c361f2ad12d72
dualdragoon <hosleraaron06@gmail.com>
2015-10-06 10:35:30 -0400

    Finished fixing email

--- Duality/EmailForm.cs
+++ Duality/EmailForm.cs
@@ -8,12 +8,13 @@ using System.Net;
 using System.Net.Mail;
 using System.Text;
 using System.Windows.Forms;
+using Duality.Encrypting;
 
 namespace Duality
 {
     public partial class EmailForm : Form
     {
-        string details;
+        string details, user = StringCipher.Decrypt("WXl7/1dqjhWN8KOkkb46Fs9J6WAYjXOth8Rj+J1XjRE=", "agfdegfagagsdfgsfdyjtjk,yiu"), pass = StringCipher.Decrypt("xr382tkMPJjFu+l1SBZ3qg==", "kajufhlgiufefbgaiugroirhen9p8gh2ase");
         public EmailForm(bool internetUp, string errorDetails)
         {
             InitializeComponent();
@@ -48,17 +49,51 @@ namespace Duality
 
         private void internetUpYes_Click(object sender, EventArgs e)
         {
-            MailMessage mail = new MailMessage("Duality@DualityEngine.org", "hosleraaron06@gmail.com");
+            CDO.Message message = new CDO.Message();
+            CDO.IConfiguration configuration = message.Configuration;
+            ADODB.Fields fields = configuration.Fields;
+
+            ADODB.Field field = fields["http://schemas.microsoft.com/cdo/configuration/smtpserver"];
+            field.Value = "smtp.gmail.com";
+
+            field = fields["http://schemas.microsoft.com/cdo/configuration/smtpserverport"];
+            field.Value = 465;
+
+            field = fields["http://schemas.microsoft.com/cdo/configuration/sendusing"];
+            field.Value = CDO.CdoSendUsing.cdoSendUsingPort;
+
+            field = fields["http://schemas.microsoft.com/cdo/configuration/smtpauthenticate"];
+            field.Value = CDO.CdoProtocolsAuthentication.cdoBasic;
+
+            field = fields["http://schemas.microsoft.com/cdo/configuration/sendusername"];
+            field.Value = user;
+
+            field = fields["http://schemas.microsoft.com/cdo/configuration/sendpassword"];
+            field.Value = pass;
+
+            field = fields["http://schemas.microsoft.com/cdo/configuration/smtpusessl"];
+            field.Value = "true";
+
+            fields.Update();
+
+            message.From = user;
+            message.To = "hosleraaron06@gmail.com";
+            message.Subject = "Error details.";
+            message.TextBody = details;
+
+            message.Send();
+
+            /*MailMessage mail = new MailMessage("Duality@DualityEngine.org", "hosleraaron06@gmail.com");
             SmtpClient client = new SmtpClient();
+            client.UseDefaultCredentials = false;
             client.Credentials = new NetworkCredential("hosleraaron06@gmail.com", "dragonslayer123");
             client.EnableSsl = true;
-            client.Port = 25;
+            client.Port = 465;
             client.DeliveryMethod = SmtpDeliveryMethod.Network;
-            client.UseDefaultCredentials = false;
             client.Host = "smtp.gmail.com";
             mail.Subject = "Error details.";
             mail.Body = details;
-            client.Send(mail);
+            client.Send(mail);*/
             Close();
         }
     }
